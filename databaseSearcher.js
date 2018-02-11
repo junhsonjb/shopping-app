57a5861d00a40836a76c2120f65427f6f64def1d
cope1010 <jakekcope@gmail.com>
2018-02-11 08:16:02 -0500

    databaseSearcher finalization

--- databaseSearcher.js
+++ databaseSearcher.js
@@ -1,6 +1,9 @@
+var stores = [];
 var attributes = [];
-var validStores = [];
-var attr = "";
+var storeScores = [];
+var links = [];
+
+var attribGoals = [];
 
 // Initialize Firebase
 var config = {
@@ -13,28 +16,78 @@ var config = {
 };
 firebase.initializeApp(config);
 
-var database = firebase.database();
+function loadData(attribs) {
+  attribGoals = attribs;
+  var database = firebase.database();
+  var ref = database.ref('stores');
+  ref.on('value', gotData, errData);
+}
 
 function gotData(data) {
-	var db = data.val();
-	var sto = Object.keys(db);
-	for (var i = 0; i < sto.length; i++) { 
-		var store = sto[i];
-		var att = db[store].attributes;
-		if (att.includes(attr)) {
-			validStores.push(store);
-			attributes.push(att);
-		}
-	}
-	console.log("The following stores have the \"" + attr + "\" attribute: " + validStores);
+  stores = [];
+  attributes = [];
+  links = [];
+  var db = data.val();
+  var sto = Object.keys(db);
+  for (var i = 0; i < sto.length; i++) {
+    var store = sto[i];
+    stores.push(store);
+    attributes.push(db[store].attributes);
+    links.push(db[store].url);
+  }
+  showResults();
 }
 
 function errData(data) {
-		console.log("Cannot find stores in database.");
+  console.log("Cannot find stores in database.");
 }
 
-function getStores(attrib) {
-  	attr = attrib;
-  	var ref = database.ref('stores');
-		ref.on('value', gotData, errData);
+function getScores(attribs) {
+  for (var i = 0; i < stores.length; ++i) {
+    storeScores[i] = 0;
+  }
+  for (var i = 0; i < attribs.length; i++) {
+    for (var j = 0; j < stores.length; j++) { 
+      if (attributes[j].includes(attribs[i])) {
+        storeScores[j]++;
+      }
+    }
+  }
+}
+
+function showResults() {
+    var attribs = attribGoals;
+    getScores(attribs);
+    var newStores = [];
+    var newLinks = [];
+    for (var i = 0; i < 6; i++) {
+      var maxScore = 0;
+      var maxStore = null;
+      var maxUrl = null;
+      for (var j = stores.length-1; j >= 0; j--) {
+        if (storeScores[j] > maxScore) {
+          if (maxStore != null) {
+            stores.push(maxStore);
+            scores.push(maxScore);
+            links.push(maxUrl);
+          }
+          maxScore = storeScores[j];
+          maxStore = stores[j];
+          maxUrl = links[j];
+          storeScores.splice(j, 1);
+          scores.splice(j, 1);
+          links.splice(j, 1);
+        }
+      }
+      newStores.push(maxStore);
+      newLinks.push(maxUrl);
+    }
+    stores = newStores;
+    links = newLinks;
+    for (var i = 1; i <= stores.length; i++) {
+      var element = document.getElementById("l"+i);
+      element.href = links[i-1];
+      var button = document.getElementById("b"+i);
+      element.value = stores[i-1];
+    }
 }
